ad482b705ac07777a156ec49e2b7dabe2dfdbf6e
yadavgopinath <yadavgopinath93@gmail.com>
2024-10-19 01:20:43 +0530

    Expence-tracker -Main-step-11

--- Expense tracker-Final/controllers/premiumfeature.js
+++ Expense tracker-Final/controllers/premiumfeature.js
@@ -1,29 +1,28 @@
 const Expenses = require('../models/expenses');
 const User = require('../models/users');
+const Sequelize = require('sequelize');
 
 const sequalize = require('../util/database');
 
 exports.getUserLeaderBoard = async (req,res,next)=>{
     try{
-        const users = await User.findAll();
-        const expenses =await Expenses.findAll();
-         const userAggregateexpenses = {};
-         expenses.forEach((expense) => {
-            if(userAggregateexpenses[expense.userId])
+        const leaderBoard = await User.findAll({
+        attributes: [
+            'id',
+            'name',
+            [Sequelize.fn('sum', Sequelize.col('expenses.amount')), 'total_cost'] 
+        ],
+        include: [
             {
-                userAggregateexpenses[expense.userId]=userAggregateexpenses[expense.userId] + expense.amount;
-                        }else{
-              userAggregateexpenses[expense.userId] = expense.amount;      
-                        }      
-         });
-         var leaderBoard =[];
-         users.forEach((user)=>{
-                  leaderBoard.push({name:user.name,total_cost:userAggregateexpenses[user.id] ||0});
-         })
-         leaderBoard.sort((a,b)=>b.total_cost-a.total_cost);
-         console.log(leaderBoard);
-         res.status(200).json(leaderBoard);
+                model: Expenses, 
+                attributes: []   
+            }
+        ],
+        group: ['users.id'],
+        order: [[Sequelize.fn('sum', Sequelize.col('expenses.amount')), 'DESC']] 
+    });
 
+    res.status(200).json(leaderBoard); 
     }catch(err){
         res.status(501).json(err);
     }
